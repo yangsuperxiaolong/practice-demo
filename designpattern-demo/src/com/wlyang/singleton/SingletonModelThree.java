b32068b9975bf5528ebe4e1a0c3ef7a0879fb39e
wlyang <yangsuperxiaolong@163.com>
2019-12-18 17:51:46 +0800

    singleton demo -to be revise

---
+++ designpattern-demo/src/com/wlyang/singleton/SingletonModelThree.java
@@ -0,0 +1,24 @@
+package com.wlyang.singleton;
+
+/**
+ * @Description: 单例模式创建方式1
+ * @author: wlyang
+ * @since: 2019-12-18
+ */
+public class SingletonModelThree {
+    /**
+     * 创建单例模式需要考虑的因素
+     * 1.即时- 懒 加载
+     * 2.线程是否安全
+     * 3.效率
+     */
+    private static SingletonModelThree instance=null;
+    private  SingletonModelThree(){
+    }
+    public static SingletonModelThree getInstance(){
+        if(instance==null) {
+            instance = new SingletonModelThree();
+        }
+        return instance;
+    }
+}
